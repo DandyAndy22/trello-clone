e12e60ded56bc1289382e588d4548e1b58591e5e
Andras Mihaly <andrasm96@gmail.com>
2026-01-24 11:21:10 -0800

    Add User model

---
+++ backend/app/models/user.rb
@@ -0,0 +1,14 @@
+class User < ApplicationRecord
+  has_secure_password
+  has_many :boards, dependent: :destroy
+
+  validates email, presence: true, uniqueness: true, format: { with: URI::MailTo::EMAIL_REGEXP }
+  validates :name, presence: true
+  validates :password, length: { minimum: 6 }, if: :password_required?
+
+  private
+
+  def password_required?
+    password_digest.nil? || password.present?
+  end
+end
